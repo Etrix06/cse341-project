92170739dd89702a4c706c42c038e66a9c0c108c
etrix06 <alc18005@byui.edu>
2021-09-18 10:08:09 -0700

    line 38 of prove01 routes was     res.write('<body><ul><li>User 1</li>'); changed to line 38-41

--- prove01-routes.js
+++ prove01-routes.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
-  
+
 
   if (url === '/') {
     res.write('<html>');
@@ -35,7 +35,11 @@ const requestHandler = (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Users</title><head>');
-    res.write('<body><ul><li>User 1</li>');
+    res.write('<body><ul>');  //<li>User 1</li>');
+    for (const activity of activities) {
+      res.write(`<li>${activity}</li>`);
+    }
+
     res.write('<li>User 2</li></ul></body>');
     res.write('</html');
     res.end();
