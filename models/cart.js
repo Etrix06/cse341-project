86d50443420fa91a6cdb7c62635300bea1da943b
etrix06 <alc18005@byui.edu>
2021-10-03 00:14:34 -0700

    Finished Academind module

--- models/cart.js
+++ models/cart.js
@@ -16,7 +16,7 @@ module.exports = class Cart {
         cart = JSON.parse(fileContent);
       }
       // Analyze the cart => Find existing product
-      const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+      const existingProductIndex = cart.products.findIndex(prod => prod.id === id);// loops thorugh all products until it find product that exactly matches id.
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
       // Add new product/ increase quantity
@@ -37,4 +37,38 @@ module.exports = class Cart {
       });  
     });
   }
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = {...JSON.parse(fileContent)};
+      const product = updatedCart.products.find(prod => prod.id === id);
+      if (!product) {
+        return;
+      }
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(
+        prod => prod.id !== id
+      );
+      updatedCart.totalPrice = 
+      updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
+    });
+  }
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }   
+    });
+  }
+
 };
