0b6e598b409bae4fbf18773ca32e8136bf4948c6
etrix06 <alc18005@byui.edu>
2021-10-10 22:53:07 -0700

    copied files from node

--- app.js
+++ app.js
@@ -1,25 +1,67 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
-
+const User = require('./models/user');
+const cors = require('cors');
 const app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const corsOptions = {
+  origin: "https://https://project341.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://etrix06:88qunwMxH3OSBV2s@cse341cluster-3dwlw.mongodb.net/alcalashop?retryWrites=true&w=majority";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('6163d07e5cbc4e9a6d7d71a7')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(process.env.PORT || 3000);
-console.log('listening on 3000');
+mongoose
+  .connect(
+    'mongodb+srv://etrix06:88qunwMxH3OSBV2s@cluster0.x7q6a.mongodb.net/alcalashop?retryWrites=true&w=majority')
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Erik',
+          email: 'erik@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(process.env.PORT || 3000);
+    console.log('listening on 3000');
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
