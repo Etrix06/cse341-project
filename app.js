e05bf7e02d08b6b6bf7ed779485765daa8844e5e
etrix06 <alc18005@byui.edu>
2021-10-17 13:27:09 -0700

    copied from heroku

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 const cors = require('cors');
+
+const MONGODB_URI =   'mongodb+srv://etrix06:88qunwMxH3OSBV2s@cluster0.x7q6a.mongodb.net/alcalashop';
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
+
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const corsOptions = {
-  origin: "https://https://project341.herokuapp.com/",
+  origin: "https://alcala-project341.herokuapp.com/",
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
@@ -27,37 +42,45 @@ const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://etrix06:88qunwMxH3
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({ 
+    secret: 'my secret',   /* This is not the most secure way, needs to be more random */
+    resave: false, 
+    saveUninitialized: false, 
+    store: store 
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
-  User.findById('6163d07e5cbc4e9a6d7d71a7')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+  .then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
+app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://etrix06:88qunwMxH3OSBV2s@cluster0.x7q6a.mongodb.net/alcalashop?retryWrites=true&w=majority')
+    MONGODB_URI
+  )
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Erik',
-          email: 'erik@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(process.env.PORT || 3000);
     console.log('listening on 3000');
   })
